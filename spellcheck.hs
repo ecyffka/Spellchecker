0632b0a9ce9771cc35b7f69680b10a9f124fc31e
erinc <w3y7g@ugrad.cs.ubc.ca>
2023-02-13 20:11:55 -0800

    if in dictionary, skip, else replace w 'boop'

--- spellcheck.hs
+++ spellcheck.hs
@@ -1,14 +1,12 @@
-
+main :: IO [Char]
 main = do
     dictionaryString <- readFile "dictionary.txt"
     let dictionary = lines dictionaryString
     putStrLn "Enter text below"
     input <- getLine
     let addSpaces = foldr (\h t -> if h `elem` [',','.','?','"','(',')','!','/',';',':'] then (' ':h:t) else (h:t)) "" input
-    putStrLn (show addSpaces)
     let split = splitOnSpace [] "" addSpaces
-    putStrLn (show split)
-    let corrected = checkWords split
+    let corrected = checkWords [] dictionary split
     return (tail (reassemble corrected))
 
 splitOnSpace :: [[Char]] -> [Char] -> [Char] -> [[Char]]
@@ -20,8 +18,17 @@ splitOnSpace lst part (h:t) =
     else 
         splitOnSpace (lst ++ [part]) "" t
 
-checkWords :: [[Char]] -> [[Char]]
-checkWords (h:t) = (h:t)
+checkWords :: [[Char]] -> [[Char]] -> [[Char]] -> [[Char]]
+checkWords lst _ [] = lst
+checkWords lst dict (h:t) =
+    if elem h (dict++[",",".","?",['"'],"(",")","!","/",";",":"])
+        then checkWords (lst++[h]) dict t
+    else
+        let correct = getSuggestions h
+        in checkWords (lst++[correct]) dict t
+
+getSuggestions :: [Char] -> [Char]
+getSuggestions w = "boop"
 
 reassemble :: [[Char]] -> [Char]
 reassemble lst =  foldr (\h t -> if not (h `elem` [",",".","?",['"'],"(",")","!","/",";",":"]) then ((' ':h)++t) else (h++t)) "" lst

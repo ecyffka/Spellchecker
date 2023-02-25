154a61cd1ba71c707a297335ad5c0c947cbd99ba
erinc <w3y7g@ugrad.cs.ubc.ca>
2023-02-25 10:01:38 -0800

    documented code

--- spellcheck.hs
+++ spellcheck.hs
@@ -1,3 +1,8 @@
+{-
+This is the main function which gets called from the command line. It prompts the user
+for text input, parses it, runs a check against a dictionary, and for each word not found,
+provides the user with suggested corrections ("boop" as a placeholder)
+-}
 main :: IO [Char]
 main = do
     dictionaryString <- readFile "dictionary.txt"
@@ -9,6 +14,13 @@ main = do
     let corrected = checkWords [] dictionary split
     return (tail (reassemble corrected))
 
+{-
+This splits the user's input into "words" (either actual words or punctuation to
+be retaining the final output). Input: lst ([[Char]], the list of words seen so far), 
+part ([Char], the word currently being parsed), and the portion of the input string
+that has not yet been parsed ([Char])). Output: [[Char]] the list of words contained
+in the input string
+-}
 splitOnSpace :: [[Char]] -> [Char] -> [Char] -> [[Char]]
 splitOnSpace lst part "" = if (length part) > 0 then (lst ++ [part]) else lst
 splitOnSpace lst part (h:t) = 
@@ -18,6 +30,10 @@ splitOnSpace lst part (h:t) =
     else 
         splitOnSpace (lst ++ [part]) "" t
 
+{-
+Checks each word in the list of input words against a preloaded dictionary. If a given
+word is not the dictionary, it calls getSuggestions to get suggested correct words.
+-}
 checkWords :: [[Char]] -> [[Char]] -> [[Char]] -> [[Char]]
 checkWords lst _ [] = lst
 checkWords lst dict (h:t) =
@@ -27,11 +43,21 @@ checkWords lst dict (h:t) =
         let correct = getSuggestions h
         in checkWords (lst++[correct]) dict t
 
+{-
+This is a placeholder function to call the wordSuggester module for suggestions
+for a given word. It currently returns "boop" instead.
+-}
 getSuggestions :: [Char] -> [Char]
 getSuggestions w = "boop"
 
+{-
+This puts the correct list of words back together to return to the user.
+-}
 reassemble :: [[Char]] -> [Char]
 reassemble lst =  foldr (\h t -> if not (h `elem` [",",".","?",['"'],"(",")","!","/",";",":"]) then ((' ':h)++t) else (h++t)) "" lst
 
--- https://github.com/dwyl/english-words
--- https://hackage.haskell.org/package/text-2.0.1/docs/Data-Text-IO.html
+{-
+References:
+https://github.com/dwyl/english-words
+https://hackage.haskell.org/package/text-2.0.1/docs/Data-Text-IO.html
+-}
